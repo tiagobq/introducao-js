32de360f3ba1abbe047999e3920ab737f1fd7256
Tiago Sousa <159965545+tiagobq@users.noreply.github.com>
2024-08-24 20:26:02 -0300

    operadores

---
+++ js/segregacao.js
@@ -0,0 +1,34 @@
+
+    const olaMundo = 'Olá mundo, introdução a JS';
+    console.log(olaMundo);
+    console.log(`Data hora: ${new Date()}`);
+
+    function mensagem(mensagem){
+        alert(mensagem);
+            }
+
+    function mensagem2(){
+        mensagem('Aprendendo funções');
+    }
+
+    const mensagemFinal = (mensagemFinal) =>{
+        console.log(mensagemFinal);
+    }
+
+    const verdadeiro = true;
+    const naoVerdadeiro = !verdadeiro;
+
+    const tudoEVerdadeiro = verdadeiro && naoVerdadeiro;
+    const tudoEFalso = !verdadeiro && naoVerdadeiro;
+    const algumEVerdadeiro = verdadeiro || naoVerdadeiro;
+    const algumEFalso = verdadeiro || naoVerdadeiro;
+
+    console.log('verdadeiro', verdadeiro);
+    console.log('naoVerdadeiro', naoVerdadeiro);
+    console.log('tudoEVerdadeiro', tudoEVerdadeiro);
+    console.log('tudoEFalso', tudoEFalso);
+    console.log('algumEVerdadeiro', algumEVerdadeiro);
+    console.log('algumEFalso', algumEFalso);
+
+  
+
