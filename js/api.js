04788b91b6c809bf99b775158112cdaf683f3d53
Tiago Sousa <159965545+tiagobq@users.noreply.github.com>
2024-09-12 20:17:45 -0300

    aplicação

--- js/api.js
+++ js/api.js
@@ -1,5 +1,15 @@
+const gatosDiv = $('#gatos');
+
 function obterGato(){
     fetch('https://cataas.com/cat')
-    .then(resultado => console.log(resultado.body))
-    .catch(error => console.log('Erro ao executar API', ERROR));
+    .then(resultado => resultado.blob())
+    .then(blob => {
+        const imageObjectURL = URL.createObjectURL(blob);
+
+        const image = document.createElement('img')
+        image.src = imageObjectURL
+        gatosDiv.append(image);
+        
+    })
+    .catch(error => console.log('Erro ao executar API', error));
 }
